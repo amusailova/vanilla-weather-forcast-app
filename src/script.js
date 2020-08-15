c47afd7206fa42679dafe2275fc7514249d53a27
amusailova <amusailova@users.noreply.github.com>
2020-08-15 17:25:55 -0400

    Created a search form

--- src/script.js
+++ src/script.js
@@ -47,8 +47,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-apiKey = "1596a4fb887b619cbb2b5ab4524f4fc0";
-let city = "Miami";
-apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1596a4fb887b619cbb2b5ab4524f4fc0";
+  apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
